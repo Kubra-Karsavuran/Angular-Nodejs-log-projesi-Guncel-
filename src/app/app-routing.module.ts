d79916083639db9b766c754618774526df142618
Kübra Karsavuran <kubrakarsavuran18@gmail.com>
2023-04-28 16:33:37 +0300

    ana sayfa duzenlendı

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnaComponent } from './ana/ana.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ana', component: AnaComponent },
+  // alttakı kısım router kısmının calısması ıcın olması gereken bır koddur
+  { path: '', redirectTo: 'ana', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
